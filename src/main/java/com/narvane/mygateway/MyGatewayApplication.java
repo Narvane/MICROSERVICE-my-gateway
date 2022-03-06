4bdccb5ae72891caa030fbb2812938cf8959dee1
Narvane <gustavoalvesandrade18@gmail.com>
2022-03-06 18:30:52 -0300

    Integration KeyCloak authorization server

--- src/main/java/com/narvane/mygateway/MyGatewayApplication.java
+++ src/main/java/com/narvane/mygateway/MyGatewayApplication.java
@@ -4,6 +4,7 @@ import com.narvane.mygateway.properties.RoutesProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -16,17 +17,29 @@ public class MyGatewayApplication {
 	@Autowired
 	private RoutesProperties routesProperties;
 
+	@Autowired
+	private TokenRelayGatewayFilterFactory filterFactory;
+
 	@Bean
 	public RouteLocator myRouter(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(p -> p
 						.path("/my-school/**")
+						 .filters(f -> f
+                             .filter(filterFactory.apply())
+                             .removeRequestHeader("Cookie"))
 						.uri(routesProperties.getUrlMySchool()))
 				.route(p -> p
 						.path("/my-user/**")
+						.filters(f -> f
+                                .filter(filterFactory.apply())
+                                .removeRequestHeader("Cookie"))
 						.uri(routesProperties.getUrlMyUser()))
 				.route(p -> p
 						.path("/my-report/**")
+						.filters(f -> f
+                                .filter(filterFactory.apply())
+                                .removeRequestHeader("Cookie"))
 						.uri(routesProperties.getUrlMyReport()))
 				.build();
 	}
